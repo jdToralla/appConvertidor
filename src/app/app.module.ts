80b3c82d550611809cb9c6972344be9044fb6c9a
jdToralla <vos2madre@gmail.com>
2021-04-10 19:18:33 -0600

    App calculo de areas y perimetros

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule  } from "@angular/forms";
+
+//* Imports Angular Material
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatButtonModule} from '@angular/material/button';
+import {NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
+import {MatExpansionModule} from '@angular/material/expansion';  
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'; 
+import {MatTableModule} from '@angular/material/table';
+import {MatRadioModule} from '@angular/material/radio'; 
+import {MatCardModule} from '@angular/material/card'; 
+import {ClipboardModule} from '@angular/cdk/clipboard'; 
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NoopAnimationsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    NgbModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatRadioModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    ClipboardModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
